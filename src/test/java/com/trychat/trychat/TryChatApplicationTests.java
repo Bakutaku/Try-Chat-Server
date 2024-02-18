4bbaf564db8b27bddd8805c5c5c41217f92c9560
Bakutaku <133964557+Bakutaku@users.noreply.github.com>
2024-02-18 14:39:56 +0900

    Hello World

---
+++ src/test/java/com/trychat/trychat/TryChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trychat.trychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
